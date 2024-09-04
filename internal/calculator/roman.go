c38b66f061954eb7e627e8a147c2853f9c667922
somnium11 <domashich1996@gmail.com>
2024-09-05 01:32:52 +0300

    added Romanian L(50) and C(100) for correct operations

--- internal/calculator/roman.go
+++ internal/calculator/roman.go
@@ -1,37 +1,47 @@
 package calculator
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 var romanToArabicMap = map[string]int{
-    "I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6,
-    "VII": 7, "VIII": 8, "IX": 9, "X": 10,
+	"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6,
+	"VII": 7, "VIII": 8, "IX": 9, "X": 10, "L": 50, "C": 100,
 }
 
 var arabicToRomanMap = []struct {
-    Value  int
-    Symbol string
+	Value  int
+	Symbol string
 }{
-    {10, "X"},
-    {9, "IX"},
-    {5, "V"},
-    {4, "IV"},
-    {1, "I"},
+	{100, "C"},
+	{50, "L"},
+	{10, "X"},
+	{9, "IX"},
+	{8, "VIII"},
+	{7, "VII"},
+	{6, "VI"},
+	{5, "V"},
+	{4, "IV"},
+	{3, "III"},
+	{2, "II"},
+	{1, "I"},
 }
 
 func RomanToArabic(roman string) (int, error) {
-    if value, exists := romanToArabicMap[roman]; exists {
-        return value, nil
-    }
-    return 0, errors.New("некорректное римское число")
+	if value, exists := romanToArabicMap[roman]; exists {
+		return value, nil
+	}
+	return 0, errors.New("некорректное римское число")
 }
 
 func ArabicToRoman(num int) string {
-    var result string
-    for _, entry := range arabicToRomanMap {
-        for num >= entry.Value {
-            result += entry.Symbol
-            num -= entry.Value
-        }
-    }
-    return result
+	var result strings.Builder
+	for _, entry := range arabicToRomanMap {
+		for num >= entry.Value {
+			result.WriteString(entry.Symbol)
+			num -= entry.Value
+		}
+	}
+	return result.String()
 }
